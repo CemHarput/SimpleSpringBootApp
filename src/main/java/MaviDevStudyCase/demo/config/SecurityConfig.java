9e2f8ef8425c03f2e915241dc9dff22868d1e400
CemHarput <cemharput@gmail.com>
2022-04-17 18:17:09 +0300

    Login Fixed

--- src/main/java/MaviDevStudyCase/demo/config/SecurityConfig.java
+++ src/main/java/MaviDevStudyCase/demo/config/SecurityConfig.java
@@ -2,27 +2,27 @@ package MaviDevStudyCase.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http.csrf().disable();
 
-        http.exceptionHandling().authenticationEntryPoint(new Http403ForbiddenEntryPoint());
+
 
         http
                 .httpBasic()
@@ -31,14 +31,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/list/**").permitAll()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/form/**").hasAnyRole("ADMIN")
+                .antMatchers("/form/**").permitAll()
                 .anyRequest().authenticated()
-                .and()
-                .exceptionHandling()
                 .and()
                 .formLogin()
                 .loginPage("/login")
                 .permitAll();
+
+
 
     }
 
